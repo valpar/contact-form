65bbee24f9acc4bdf6fabb5733c905a95a757422
Valmar <valmarparts@gmail.com>
2024-05-26 19:08:12 +0300

    Remove svgs, add background color and update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-blue': 'rgb(167 208 216)',
+      },
+      fontFamily: {
+        RecklessNeue: [
+          'RecklessNeue',
+          'ui-serif',
+          'Georgia',
+          'Cambria',
+          'Times New Roman',
+          'Times, serif',
+        ],
+      },
+      fontWeight: {
+        'extra-light': 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        'semi-bold': 600,
+        bold: 700,
+        'extra-bold': 800,
+        black: 900,
+      },
+    },
   },
   plugins: [],
 };
