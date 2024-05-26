9049520482e8e5aab7e849d30b36ee9cf60ed660
valmar parts <valmarparts@gmail.com>
2024-05-27 01:20:16 +0300

    Add initial layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import ContactForm from './components/ContactForm';
 
 function App() {
-  return <div className='bg-brand-blue min-h-screen overflow-auto'></div>;
+  return (
+    <div className='bg-brand-blue min-h-screen overflow-auto '>
+      <ContactForm></ContactForm>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+
+export default function ContactForm() {
+  return (
+    <div className='container-tuum-padded-x relative'>
+      {' '}
+      <div className='grid grid-cols-1 mt-[6%] tall:mt-[3%] tall-md:mt-[6%] tall:pb-[3%] pb-[6%] lg:[grid-template-columns:35%_65%] sm:px-10 md:px-16 lg:px-10 xl:px-20'>
+        <aside>
+          <div className='space-y-4 pb-7 text-center sm:text-left md:space-y-7 lg:pb-0'>
+            <p className='text-xl md:font-RecklessNeue font-extralight md:text-4xl'>
+              Media enquiries:
+            </p>
+            <a
+              href='#'
+              className='underline-link text-lg after:bottom-0.5 after:w-full after:bg-brand-black hover:after:w-0 hover:after:bg-transparent focus-visible:rounded focus-visible:outline focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-brand-black/50'
+            >
+              press@tuum.com
+            </a>
+          </div>
+          <div>
+            <p className='md:font-RecklessNeue font-normal md:text-4xl'>
+              Career questions:
+            </p>
+            <a
+              href=''
+              className='underline-link text-lg after:bottom-0.5 after:w-full after:bg-brand-black hover:after:w-0 hover:after:bg-transparent focus-visible:rounded focus-visible:outline focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-brand-black/50'
+            >
+              careers@tuum.com
+            </a>
+          </div>
+        </aside>
+        <div className='bg-white tall:py-6 tall-md:py-8 py-8 px-8 lg:px-10 xl:px-16 rounded-[40px]'>
+          <div className='wrap'>
+            <form action=''>
+              <fieldset className='form-columns-0'>
+                <div>
+                  <h1>Contact us</h1>
+                </div>
+              </fieldset>
+              <fieldset className='form-columns-2'>
+                <div>
+                  <label htmlFor=''>
+                    <span>First name</span>
+                  </label>
+                  <div className='input'>
+                    <input type='text'></input>
+                  </div>
+                  <ul
+                    className='no-list hs-error-msgs inputs-list'
+                    role='alert'
+                  >
+                    <li>
+                      <label className='hs-error-msg hs-main-font-element'>
+                        Please complete this required field.
+                      </label>
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <label htmlFor=''>
+                    <span>Last name</span>
+                  </label>
+                  <div className='input'>
+                    <input type='text'></input>
+                  </div>
+                  <ul
+                    className='no-list hs-error-msgs inputs-list'
+                    role='alert'
+                  >
+                    <li>
+                      <label className='hs-error-msg hs-main-font-element'>
+                        Please complete this required field.
+                      </label>
+                    </li>
+                  </ul>
+                </div>
+              </fieldset>
+              <fieldset className='form-columns-2'>
+                <div>
+                  <label htmlFor=''>
+                    <span>Email</span>
+                  </label>
+                  <div className='input'>
+                    <input type='text'></input>
+                  </div>
+                  <ul
+                    className='no-list hs-error-msgs inputs-list'
+                    role='alert'
+                  >
+                    <li>
+                      <label className='hs-error-msg hs-main-font-element'>
+                        Please complete this required field.
+                      </label>
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <label htmlFor=''>
+                    <span>Job title</span>
+                  </label>
+                  <div className='input'>
+                    <input type='text'></input>
+                  </div>
+                  <ul
+                    className='no-list hs-error-msgs inputs-list'
+                    role='alert'
+                  >
+                    <li>
+                      <label className='hs-error-msg hs-main-font-element'>
+                        Please complete this required field.
+                      </label>
+                    </li>
+                  </ul>
+                </div>
+              </fieldset>
+              <fieldset className='form-columns-2'></fieldset>
+              <fieldset className='form-columns-1'></fieldset>
+              <fieldset className='form-columns-1'></fieldset>
+              <fieldset className='form-columns-1'></fieldset>
+              <fieldset className='form-columns-1'></fieldset>
+              <div className='submit'></div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
