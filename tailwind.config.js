6d21655bb57a336cd8a74908454133508922dccf
Valmar <valmarparts@gmail.com>
2024-06-04 17:02:48 +0300

    Add more custom classes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,26 @@ export default {
       lineHeight: {
         15: '3.75rem',
       },
+      screens: {
+        tall: { raw: '(min-height: 600px)' },
+        'tall-md': { raw: '(min-height: 900px)' },
+      },
+      spacing: {
+        '6p': '6%', // 6% spacing
+        '3p': '3%', // 3% spacing
+        '20px': '20px',
+        '70px': '70px',
+        '102px': '102px',
+        '108px': '108px',
+        '172px': '172px',
+        '52px': '52px',
+      },
+      borderRadius: {
+        '40px': '40px',
+      },
+      gridTemplateColumns: {
+        '35_65': '35% 65%',
+      },
     },
   },
   plugins: [],
