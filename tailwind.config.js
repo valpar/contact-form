ea2adf3ad57632b56ca7725d60b5d85b24394b7c
Valmar <valmarparts@gmail.com>
2024-06-12 16:12:51 +0300

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        reckless: ['Reckless Neue', 'sans-serif'],
+        'denim-light': ['Denim-Light', 'sans-serif'],
+        'denim-regular': ['Denim-Regular', 'sans-serif'],
+        'denim-semibold': ['Denim-SemiBold', 'sans-serif'],
+      },
       colors: {
         'brand-blue': '#A7D0D8',
         'link-blue': '#219dfb',
@@ -19,19 +25,19 @@ export default {
         'tall-md': { raw: '(min-height: 900px)' },
       },
       spacing: {
-        '6p': '6%', // 6% spacing
-        '3p': '3%', // 3% spacing
-        '20px': '20px',
-        '70px': '70px',
-        '102px': '102px',
-        '108px': '108px',
-        '172px': '172px',
-        '52px': '52px',
+        '6p': '1.5rem', // 6% spacing
+        '3p': '0.75rem', // 3% spacing
+        '20px': '1.25rem',
+        '70px': '4.375rem',
+        '102px': '6.375rem',
+        '108px': '6.75rem',
+        '172px': '10.75rem',
+        '52px': '3.25rem',
         2.5: '0.625rem',
         4.5: '1.125rem',
       },
       borderRadius: {
-        '40px': '40px',
+        '40px': '2.5rem',
       },
       gridTemplateColumns: {
         '35_65': '35% 65%',
